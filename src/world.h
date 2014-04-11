558020285bc042db549d740d11b72deb10f63c20
Ben Eills <ben@beneills.com>
2014-04-12 00:53:22 +0100

    * time * node equality testing

--- src/world.h
+++ src/world.h
@@ -4,6 +4,11 @@
 #include "map.h"
 #include "fauna.h"
 
+// forward declaration TODO
+struct fauna_s; typedef struct fauna_s fauna;
+
+typedef unsigned int time;
+
 typedef struct {
   map* m;
   fauna* f; // linked list of fauna
