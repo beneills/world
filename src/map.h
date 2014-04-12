cbd636063f869261f5c623008786d8fe05d498b6
Ben Eills <ben@beneills.com>
2014-04-12 15:01:02 +0100

    node <-> (x, y) conversion macro

--- src/map.h
+++ src/map.h
@@ -5,7 +5,7 @@
 #define MAP_EL_IMPASSABLE 0x01
 
 #define NODE_EQ(n1, n2) ((n1).x == (n2).x && (n1).y == (n2).y)
-
+#define NODE(x, y) ({.x = (x), .y = (y)})
 #define MAP_ELEMENT(m, x, y) m->data[y*m->width + x]
 #define MAP_NODE_VALID(m, x, y) (x) >= 0 && \
     (x) < m->width && \
