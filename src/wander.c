5ae0792daf845094dd2193d79d6c919ae9615af2
Ben Eills <ben@beneills.com>
2014-04-12 02:17:52 +0100

    refactor wander

--- src/wander.c
+++ src/wander.c
@@ -1,82 +1,86 @@
-#include "math.h"
+#include <math.h>
 
 #include "fauna.h"
 #include "map.h"
 #include "utility.h"
 #include "wander.h"
 
-#define M_PI		3.14159265358979323846	/* pi */
-#define RE_3_PI_I_8 0.3826834323650897717284599840303988667613445624856270414338
+// moves fauna to next node and returns that node
+node wander(world* w,
+	    wander_parameters* params,
+	    fauna* f) {
+  node n;
+  double perturbed_angle;
+  for ( unsigned int i = 0;
+	i < WANDER_MAX_PERTURBATION_ATTEMPTS;
+	++i ) {
+    // calculate a perturbed angle and corresponding (valid) node
+    perturbed_angle = (*params->perturb_angle)(f->status->wander);
+    n = node_from_angle(f->position, perturbed_angle);
 
+    // if this node is viable, move the fauna
+    if ( MAP_ELEMENT(m, n.x, n.y) == MAP_EL_EMPTY ) {
+      f->status->wander->angle = (*params->derived_angle)(s->angle,
+							  perturbed_angle);
+      fauna_move(f, n);
+      break;
+    }
+  }
 
+  return f->position;
+}
+
+// returns the (valid) node in direction of angle
+//   will return current rather than going off edge
+node node_from_angle(map* m,
+		     node current,
+		     double angle) {
+  double c = cos(angle);
+  double s = sin(angle);
+
+  int dx = ( c < -RE_3_PI_I_8 ) ? -1 :
+    ( c < RE_3_PI_I_8 ) ? 0 :
+    1;
+  int dy = ( s < -RE_3_PI_I_8 ) ? -1 :
+    ( s < RE_3_PI_I_8 ) ? 0 :
+    1;
+
+  if ( MAP_NODE_VALID(m, current.x + dx, current.y + dy) ) {
+    return {.x = (unsigned int) (current.x + dx),
+	.y = (unsigned int) (current.y + dy)
+	};
+  }
+
+  return current;		/* default */
+}
+
+// returns a random wander_status, currently just choosing
+//   the angle uniformly
+wander_status* wander_status_random() {
+  wander_status* w = (wander_status *) SAFEMALLOC(sizeof(wander_status));
+  w->angle = UNIFORM_RANDOM() * 2 * M_PI;
+  return w;
+}
+
+// convienience method to use basic implementation
+node wander_basic(world* w,
+		  fauna* f) {
+  wander_parameters* p = (wander_parameters *)
+    SAFEMALLOC(sizeof(wander_parameters));
+  p->perturb_angle = &perturb_angle_basic;
+  p->derived_angle = &derived_angle_basic;
+  
+  return wander(w, p, f);
+}
+
+// uniform perturbation
+// TODO use sensible distribution
 double perturb_angle_basic(double angle) {
-  // TODO
   double perturbation = uniform_random() * 2 * M_PI - M_PI;
   return angle + perturbation;
 }
 
+// linear interpolation
 double derived_angle_basic(double old_angle, double perturbed_angle) {
-  // TODO
   return (old_angle + perturbed_angle) / 2.0;
-}
-
-node node_from_angle(node current, double angle) {
-  double c = cos(angle);
-  double s = sin(angle);
-
-  current.x += ( c < -RE_3_PI_I_8 ) ? -1 :
-    ( c < RE_3_PI_I_8 ) ? 0 :
-    1;
-  current.y += ( s < -RE_3_PI_I_8 ) ? -1 :
-    ( s < RE_3_PI_I_8 ) ? 0 :
-    1;
-
-  return current;
-}
-
-// moves fauna to next node and returns that node
-// TODO use f->wander_status
-node wander(map* m,
-	    wander_parameters* params,
-	    wander_status* s,
-	    fauna* f) {
-  double perturbed_angle;
-  node n, current = {
-    .x = f->x,
-    .y = f->y
-  };
-  debug("1\n");
-  for ( unsigned int i = 0; i < WANDER_MAX_PERTURBATION_ATTEMPTS; ++i ) {
-    perturbed_angle = (*params->perturb_angle)(s->angle);
-    n = node_from_angle(current, perturbed_angle);
-    if ( map_valid(m, n.x, n.y) &&
-	 map_element(m, n.x, n.y) == MAP_EL_EMPTY ) {
-      // update angle
-      s->angle = (*params->derived_angle)(s->angle, perturbed_angle);
-      // update fauna position
-      f->x = n.x;
-      f->y = n.y;
-      // TODO explored
-      return n;
-    }
-  }
-  // default to staying still
-  return current;
-}
-
-// convienience method to use basic parameters
-node wander_basic(map* m,
-		  wander_status* s,
-		  fauna* f) {
-  wander_parameters p = {
-    .perturb_angle = &perturb_angle_basic,
-    .derived_angle = &derived_angle_basic
-  };
-  return wander(m, &p, s, f);
-}
-
-wander_status* wander_status_random() {
-  wander_status* w = (wander_status *) SAFEMALLOC(sizeof(wander_status));
-  w->angle = uniform_random() * 2 * M_PI;
-  return w;
 }
--- src/wander.h
+++ src/wander.h
@@ -1,11 +1,11 @@
 #ifndef WANDER_H_INCLUDED
 #define WANDER_H_INCLUDED
 
-// necessary for singly disconnected nodes 
-#define WANDER_MAX_PERTURBATION_ATTEMPTS 10
+#define WANDER_MAX_PERTURBATION_ATTEMPTS 10 /* necessary for singly disconnected nodes  */
+#define RE_3_PI_I_8 0.3826834323650897717284599840303988667613445624856270414338 /* Re{e^{3pi/8}} */
 
 typedef struct {
-  double angle; // radians
+  double angle;			/* radians, in [0, 2pi) */
 } wander_status;
 
 typedef struct {
@@ -13,5 +13,18 @@ typedef struct {
   double (*derived_angle)(double, double);
 } wander_parameters;
 
+node wander(world* w,
+	    wander_parameters* params,
+	    fauna* f);
+node node_from_angle(map* m,
+		     node current,
+		     double angle);
+wander_status* wander_status_random();
+
+// basic implementation
+node wander_basic(world* w,
+		  fauna* f);
+double perturb_angle_basic(double angle);
+double derived_angle_basic(double old_angle, double perturbed_angle);
 
 #endif
